374a7faa24c2d7886ab17383a90f1118527bd3d1
“ameybhosle212” <ameybhosle212@gmail.com>
2023-04-28 14:08:05 +0530

    Adde:Frontend+Backend

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+require('dotenv').config()
+
+
+// Middleware 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('/',require("./routes/index"))
+
+if(process.env.NODE_ENV=='production'){
+    const path = require('path')
+
+    app.get('/',(req,res)=>{
+        app.use(express.static(path.resolve(__dirname,'client','build')))
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    })
+}
+
+// Server 
+app.listen(2200,()=>{
+    console.log("Server at 2200");
+})
